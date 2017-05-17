79ea8b26b16c8abaf7eaefa2e757dcfe0b6cc751
rymoasky <rymoa781122@gmail.com>
2017-05-17 16:39:13 +0800

    20170517.rong01.php

---
+++ rong01.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: user
+ * Date: 2017/5/17
+ * Time: 下午 03:56
+ */
+echo 'Hello! World<br>';
+echo 'Hello! World';
+
+$var1=123;
+$a=123.5;
+$___=ture;
+$_123='rmmoa';
+echo gettype($var1);
+echo gettype($_123);
+?>
+
+
