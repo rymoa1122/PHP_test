72b60679b9205401057f7c99c373b69bdbf762dc
rymoasky <rymoa781122@gmail.com>
2017-05-19 16:32:38 +0800

    20170517.rong01.php

---
+++ rong03.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: user
+ * Date: 2017/5/19
+ * Time: 下午 02:37
+ */
+$x = $_GET['xx'];
+$y = $_GET['yy'];
+$r = $x + $y ;
+echo "$x + $y = $r" ;
+
+$var1 ="30";$var2="20";
+$var3= $var1+$var2;
+var_dump($var3);
+
+
---
+++ rymoasky.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: user
+ * Date: 2017/5/19
+ * Time: 下午 03:12
+ */
+
