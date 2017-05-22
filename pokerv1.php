b6c8655fa4b22b9bc93f9a2fcc80b9a6c0e82905
rymoasky <rymoa781122@gmail.com>
2017-05-22 16:18:11 +0800

    php_test

--- pokerv1.php
+++ pokerv1.php
@@ -1,6 +1,18 @@
 <?php
-$poker =array();
-for ($i=0;$i<52;$i++){
-    $poker[]=rand(1,51);
+$poker = array();
+for ($i = 0; $i < 52 ; $i++) {
+
+do{
+    $temp = rand(0, 51);
+    $isRepeat = false;
+    for ($j = 0; $j < $i; $j++) {
+        if ($poker[$j] == $temp) {
+            $isRepeat = true;
+            break;
+        }
+    }
+
+} while ($isRepeat);
+    $poker[] = $temp;
     echo $poker[$i] . '<br>';
 }
