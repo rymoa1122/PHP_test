8c47bcad68786a09e30a036924a53133c6573059
rymoasky <rymoa781122@gmail.com>
2017-05-23 09:00:42 +0800

    php_test

--- pokerv2.php
+++ pokerv2.php
@@ -1,18 +1,18 @@
 <?php
+
 $poker = array();
+for ($i=0;$i<52;$i++)$check[]=false;
 for ($i = 0; $i < 52 ; $i++) {
 
     do{
-        $temp = rand(0, 51);
         $isRepeat = false;
-        for ($j = 0; $j < $i; $j++) {
-            if ($poker[$j] == $temp) {
-                $isRepeat = true;
-                break;
-            }
+        $temp = rand (0, 51);
+        if(!$check[$temp]){
+        $check[$temp]=true;
         }
-
+         else{
+          $isRepeat=true;
+    }
     } while ($isRepeat);
-    $poker[] = $temp;
-    echo $poker[$i] . '<br>';
+      echo $poker[$i] . '<br>';
 }
