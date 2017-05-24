8f269429f5a8a56f35982cdd8b2a6c7195eac96d
rymoasky <rymoa781122@gmail.com>
2017-05-24 10:28:45 +0800

    php_test

---
+++ rong16.php
@@ -0,0 +1,5 @@
+<form action="rong17.php" method="post">
+    Name:<input type="text" name="account"/><br>
+    Password:<input type="password" name="account"/><br>
+    <input type="submit" value="送出">
+</form>
---
+++ rong17.php
@@ -0,0 +1,5 @@
+<?php
+
+$account= $_POST['account'];
+
+echo $account;
