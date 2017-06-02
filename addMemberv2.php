c96acb182fb123f5166a79c7ec4d868d907d8259
rymoasky <rymoa781122@gmail.com>
2017-06-02 16:32:04 +0800

    php test

--- addMemberv2.php
+++ addMemberv2.php
@@ -1,6 +1,36 @@
+<script>
+    if (window.XMLHttpRequest) {
+        // code for modern browsers
+        xmlhttp = new XMLHttpRequest();
+    } else {
+        // code for old IE browsers
+        xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+
+    function precheck() {
+        var account = document.getElementById('account').value;
+        alert(account);
+        xmlhttp.onreadystatechange = callBack1;
+        xmlhttp.open("GET", "rong44.php?a="+account, true);
+        xmlhttp.send();
+    }
+    function callBack1() {
+        if (this.readyState == 4 && this.status == 200) {
+            var result = this.responseText;
+            if (result != 'OK'){
+                document.getElementById('crs').innerHTML='重複';
+            }else {
+                document.getElementById('crs').innerHTML='很好';
+            }
+        }
+    }
+</script>
 <form action="addAccount.php">
-    <input name="account" /><br>
+    <input id='account' name="account" onblur="precheck()" />
+    <span id="crs"></span><br>
     <input type="password" name="passwd" /><br>
     <input name="rname" /><br>
     <input type="submit" value="add" />
 </form>
+<hr>
+<div id="test1"></div>
---
+++ rong46.php
@@ -0,0 +1,23 @@
+<?php
+
+include "sqlRong.php";
+$pdo = new pdo($dsn, $user, $passwd, $opt);
+
+
+$sql = "SELECT * FROM member ";
+$stmt = $pdo->prepare($sql);    // statement SQL
+$stmt->execute();
+
+echo "<table>";
+while ($obj = $stmt->fetchObject()){
+    echo '<tr>';
+    echo "<td>{$obj->id}</td>";
+    echo "<td>{$obj->account}</td>";
+    echo "<td>{$obj->passwd}</td>";
+    echo '</tr>';
+}
+echo "</table>";
+
+
+
+
